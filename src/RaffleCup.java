583c86c7c869ed35e44a097dc5c737a1931776be
Sebastian Goldbach <Sig.goldbach@gmail.com>
2021-09-29 12:28:03 +0200

    Rafflcup klasse til slag

---
+++ src/RaffleCup.java
@@ -0,0 +1,12 @@
+public class RaffleCup {
+    int interning1;
+    int interning2;
+    int sum;
+    public void slå(){
+        interning1=(int) (Math.random()*6+1);
+        interning2=(int) (Math.random()*6+1);
+        sum=interning1+interning2;
+    }
+
+
+}
