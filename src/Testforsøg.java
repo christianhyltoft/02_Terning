9a66d548764f365c44474701a97b695ffff9a032
Valdemar Nielsen <v.nielsen@live.dk>
2021-09-29 14:04:25 +0200

    Made test for 1000 dice rolls with a sum-counter.

--- src/Testforsøg.java
+++ src/Testforsøg.java
@@ -4,15 +4,15 @@ public class Testforsøg {
         RaffleCup cup2 = new RaffleCup();
         Player player1 = new Player(cup1, "player1");
         Player player2 = new Player(cup1, "player2");
-        while (player1.points<40) {
+        /*while (player1.points<40) {
             cup1.roll();
             cup1.print();
             player1.redirectPoint(cup1);
             cup2.roll();
             cup2.print();
-            player2.redirectPoint(cup2);
+            player2.redirectPoint(cup2);*/
+        cup1.test();
 
         }
 
     }
-}
