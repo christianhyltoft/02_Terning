c79940f24b38a120436cc85eb6fa990bd48a166a
Sebastian Goldbach <Sig.goldbach@gmail.com>
2021-10-05 16:36:19 +0200

    Rettelse af terning så når man nu skal slå to ens ettere efter seks mister man point...

--- src/spil/java/Player.java
+++ src/spil/java/Player.java
@@ -2,28 +2,32 @@ import java.util.*;
 
 public class Player {
     final String inname;
-   final RaffleCup incup;
-   private int points;
+    final RaffleCup incup;
+    private int points;
     boolean win = false;
 
     public int getPoints() {
         return points;
     }
-    public boolean getWin(){
+
+    public boolean getWin() {
         return win;
     }
-    public void setPoint(int setPoints){
-        points=setPoints;
+
+    public void setPoint(int setPoints) {
+        points = setPoints;
     }
-    public void setWin(boolean setWin){
-        win=setWin;
+
+    public void setWin(boolean setWin) {
+        win = setWin;
     }
 
     public Player(RaffleCup cup, String name) {
         inname = name;
         incup = cup;
     }
-//Vurderer om spilleren har sloet lige eller ulige, og sender videre til andre metoder. gør intet hvis en spiller har vundet.
+
+    //Vurderer om spilleren har sloet lige eller ulige, og sender videre til andre metoder. gør intet hvis en spiller har vundet.
     public void redirectPoint(RaffleCup x) {
         if (win) {
 
@@ -35,13 +39,15 @@ public class Player {
             }
         }
     }
-//Hvis spiller har forskellige slag for man bare point
+
+    //Hvis spiller har forskellige slag for man bare point
     private void awardpoint(RaffleCup x) {
         points = points + x.getSum();
         System.out.println(inname + " You now have " + points + " Points");
 
     }
-// Hvis man får 2 ens kaldes denne metode.
+
+    // Hvis man får 2 ens kaldes denne metode.
     private void samefaces(RaffleCup x) {
         if (points >= 40) {
             if (x.getInterning1() == x.getInterning2() && x.getInterning1() != 1) {
@@ -63,7 +69,7 @@ public class Player {
                 if (x.getInterning1() == 6 && x.getInterning2() == 6) {
                     win();
                 }
-                if (x.getInterning1() == 1) {
+                if (x.getInterning1() == 1 && x.getInterning2() == 1) {
                     points = 0;
                     System.out.println(inname + " Your points have been reset, and now you have " + points + " Points");
                     reroll(x);
@@ -76,7 +82,8 @@ public class Player {
             }
         }
     }
-// Reroll til hvis man slor to ens
+
+    // Reroll til hvis man slor to ens
     private void reroll(RaffleCup x) {
         System.out.println("Congratulations you may now roll again");
         Scanner rollagain = new Scanner(System.in);
@@ -84,13 +91,15 @@ public class Player {
         x.roll();
         redirectPoint(x);
     }
-//setter win til true hvis en spiller har opfyldt en win condition.
+
+    //setter win til true hvis en spiller har opfyldt en win condition.
     private void win() {
         win = true;
         System.out.println(inname + " Has won the game");
 
     }
-//Udprinter maengden af point spilleren har.
+
+    //Udprinter maengden af point spilleren har.
     private void printpoint() {
         System.out.println(inname + " You now have " + points + " Points");
     }
